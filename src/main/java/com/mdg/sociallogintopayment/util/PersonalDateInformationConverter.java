19f7542d274ee095fb0f1abe203b7b62013057ec
DonggeonMoon <nullsafer@naver.com>
2023-03-05 20:37:53 +0900

    fix: jpa AttributeConverter 추가

--- src/main/java/com/mdg/sociallogintopayment/util/PersonalDateInformationConverter.java
+++ src/main/java/com/mdg/sociallogintopayment/util/PersonalDateInformationConverter.java
@@ -1,19 +1,65 @@
 package com.mdg.sociallogintopayment.util;
 
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
 import java.time.LocalDate;
+import java.util.Base64;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+
+import org.springframework.beans.factory.annotation.Value;
 
 import jakarta.persistence.AttributeConverter;
 import jakarta.persistence.Converter;
 
 @Converter
 public class PersonalDateInformationConverter implements AttributeConverter<LocalDate, String> {
+    @Value("${crypto.algorithm}")
+    private String algorithm;
+    @Value("${crypto.key}")
+    private String key;
+    private final byte[] iv = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
+    private final Cipher cipher;
+    private final SecretKeySpec secretKeySpec = new SecretKeySpec(key.getBytes(), "AES");
+    private final IvParameterSpec ivParameterSpec = new IvParameterSpec(iv);
+
+    public PersonalDateInformationConverter() {
+        try {
+            cipher = Cipher.getInstance(algorithm);
+        } catch (NoSuchAlgorithmException | NoSuchPaddingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
     @Override
     public String convertToDatabaseColumn(LocalDate attribute) {
-        return null;
+        try {
+            String convertedDate = String.valueOf(attribute);
+
+            cipher.init(Cipher.ENCRYPT_MODE, secretKeySpec, ivParameterSpec);
+
+            return Base64.getEncoder().encodeToString(cipher.doFinal(convertedDate.getBytes()));
+        } catch (InvalidKeyException | IllegalBlockSizeException | BadPaddingException | InvalidAlgorithmParameterException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public LocalDate convertToEntityAttribute(String dbData) {
-        return null;
+        try {
+            cipher.init(Cipher.DECRYPT_MODE, secretKeySpec, ivParameterSpec);
+
+            String decryptedDate = new String(cipher.doFinal(Base64.getDecoder().decode(dbData)));
+
+            return LocalDate.parse(decryptedDate);
+        } catch (InvalidKeyException | InvalidAlgorithmParameterException | BadPaddingException | IllegalBlockSizeException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
--- src/main/java/com/mdg/sociallogintopayment/util/PersonalInformationConverter.java
+++ src/main/java/com/mdg/sociallogintopayment/util/PersonalInformationConverter.java
@@ -1,17 +1,60 @@
 package com.mdg.sociallogintopayment.util;
 
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+
+import org.springframework.beans.factory.annotation.Value;
+
 import jakarta.persistence.AttributeConverter;
 import jakarta.persistence.Converter;
 
 @Converter
 public class PersonalInformationConverter implements AttributeConverter<String, String> {
+    @Value("${crypto.algorithm}")
+    private String algorithm;
+    @Value("${crypto.key}")
+    private String key;
+    private final byte[] iv = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
+    private final Cipher cipher;
+    private final SecretKeySpec secretKeySpec = new SecretKeySpec(key.getBytes(), "AES");
+    private final IvParameterSpec ivParameterSpec = new IvParameterSpec(iv);
+
+    public PersonalInformationConverter() {
+        try {
+            cipher = Cipher.getInstance(algorithm);
+        } catch (NoSuchAlgorithmException | NoSuchPaddingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
     @Override
     public String convertToDatabaseColumn(String attribute) {
-        return null;
+        try {
+            cipher.init(Cipher.ENCRYPT_MODE, secretKeySpec, ivParameterSpec);
+
+            return Base64.getEncoder().encodeToString(cipher.doFinal(attribute.getBytes()));
+        } catch (InvalidKeyException | IllegalBlockSizeException | BadPaddingException | InvalidAlgorithmParameterException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public String convertToEntityAttribute(String dbData) {
-        return null;
+        try {
+            cipher.init(Cipher.DECRYPT_MODE, secretKeySpec, ivParameterSpec);
+
+            return new String(cipher.doFinal(Base64.getDecoder().decode(dbData)));
+        } catch (InvalidKeyException | InvalidAlgorithmParameterException | BadPaddingException | IllegalBlockSizeException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
